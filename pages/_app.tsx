ccb1910189d61f11e45b20ecfa0f8cf858981854
yusufziyaa <yuszihoc@gmail.com>
2022-03-13 21:57:07 +0300

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+
+const settings = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  }
+}
+const theme = extendTheme({settings});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    < ChakraProvider theme={theme} >
+      <title>HOWS - DEINE FAVOURITE</title>
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </ChakraProvider >
+  )
 }
-
 export default MyApp
